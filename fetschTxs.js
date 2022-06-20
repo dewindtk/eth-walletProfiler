6a06bf8befd23e3b97708bd649d27bf1b135dec3
dewindtk <dewindtk@student.ethz.ch>
2022-06-21 00:55:25 +0200

    first commit

---
+++ fetschTxs.js
@@ -0,0 +1,144 @@
+wallets = require('./wallets.json')
+console.log(wallets)
+
+wallet = Object.keys(wallets)[0]
+wname = "WALLET_" + wallets[wallet]
+console.log(wallet, wname)
+
+const fetch = require('node-fetch');
+const NormalTxs = `https://api.etherscan.io/api?module=account&action=txlist&address=${wallet}&startblock=0&endblock=99999999&page=1&offset=9000&sort=asc&apikey=FZNRAKKI5G24D4YP61TM22FUGWSQH4JXKN`
+const InternalTxs = `https://api.etherscan.io/api?module=account&action=txlistinternal&address=${wallet}&startblock=0&endblock=99999999&page=1&offset=9000&sort=asc&apikey=FZNRAKKI5G24D4YP61TM22FUGWSQH4JXKN`
+const ERC20Txs = `https://api.etherscan.io/api?module=account&action=tokentx&address=${wallet}&page=1&offset=9000&startblock=0&endblock=99999999&sort=asc&apikey=FZNRAKKI5G24D4YP61TM22FUGWSQH4JXKN`
+const ERC721Txs = `https://api.etherscan.io/api?module=account&action=tokennfttx&address=${wallet}&page=1&offset=9000&startblock=0&endblock=99999999&sort=asc&apikey=FZNRAKKI5G24D4YP61TM22FUGWSQH4JXKN`
+const ERC1155Txs = `https://api.etherscan.io/api?module=account&action=token1155tx&address=${wallet}&page=1&offset=9000&startblock=0&endblock=99999999&sort=asc&apikey=FZNRAKKI5G24D4YP61TM22FUGWSQH4JXKN`
+const fs = require('fs')
+
+
+async function fetchNormalTxs() 
+{
+    const response = await fetch(NormalTxs)
+    const resJson = await response.json()
+    data = resJson.result
+
+    for (i=0;i<data.length;i++)
+    {
+        data[i].txType = 'Normal'
+    }
+
+    console.log(data)
+    fs.writeFile(`./${wname}/NormalTxs.json`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+
+};
+
+async function fetchInternalTxs()
+{
+
+    const response = await fetch(InternalTxs)
+    const resJson = await response.json()
+    data = resJson.result
+
+    for (i=0;i<data.length;i++)
+    {
+        data[i].txType = 'Internal'
+    }
+
+    console.log(data)
+    fs.writeFile(`./${wname}/Internal.json`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    
+}
+
+async function fetchERC20Txs() 
+{
+
+    const response = await fetch(ERC20Txs)
+    const resJson = await response.json()
+    data = resJson.result
+
+    for (i=0;i<data.length;i++)
+    {
+        data[i].txType = 'ERC20'
+    }
+
+    console.log(data)
+    fs.writeFile(`./${wname}/ERC20.json`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    
+}
+
+async function fetchERC721Txs()
+{
+
+    const response = await fetch(ERC721Txs)
+    const resJson = await response.json()
+    data = resJson.result
+
+    for (i=0;i<data.length;i++)
+    {
+        data[i].txType = 'ERC721'
+    }
+
+    console.log(data)
+    fs.writeFile(`./${wname}/ERC721Txs.json`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    
+}
+
+async function fetchERC1155Txs()
+{
+
+    const response = await fetch(ERC1155Txs)
+    const resJson = await response.json()
+    data = resJson.result
+
+    for (i=0;i<data.length;i++)
+    {
+        data[i].txType = 'ERC1155'
+    }
+
+    console.log(data)
+    fs.writeFile(`./${wname}/ERC1155Txs.json`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    
+}
+
+
+fetchNormalTxs();
+fetchInternalTxs();
+fetchERC20Txs();
+fetchERC721Txs();
+fetchERC1155Txs();
+
+
+
+
+//// Old fetch method ////
+//   fetch(kaikunNormalTxs)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(resp) {
+//         data = JSON.parse(JSON.stringify(resp.result));
+
+//         fs.writeFile('kaikunNormalTxs.json', JSON.stringify(data), (err) => {
+//             if (err) {
+//                 throw err;
+//             }
+//         });
+//     });
